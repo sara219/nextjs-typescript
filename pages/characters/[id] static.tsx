0b4365597982cb2e779027d24df02b6dbc094126
sara219 <sara97abuawwad@gmail.com>
2022-11-07 22:03:22 +0200

    build server-render website

--- pages/characters/[id] static.tsx
+++ pages/characters/[id] static.tsx
@@ -1,13 +1,8 @@
-import { Character } from '../../types'
+import { Character, GetCharacterResult } from '../../types'
 import Image from 'next/image'
 import imageLoader from '../../imageLoader'
-import { GetServerSideProps } from 'next'
-import { useRouter } from 'next/router'
 
 function CharacterPage({ character }: { character: Character }) {
-  const router = useRouter()
-  // console.log(router.query.id, '1111')
-
   return (
     <div>
       <h1>{character.name}</h1>
@@ -23,12 +18,23 @@ function CharacterPage({ character }: { character: Character }) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export async function getStaticPaths() {
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+  const { results }: GetCharacterResult = await res.json()
+
+  return {
+    paths: results.map((character) => {
+      return { params: { id: String(character.id) } }
+    }),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }: { params: { id: String } }) {
   const res = await fetch(
-    `https://rickandmortyapi.com/api/character/${context.query.id}`
+    `https://rickandmortyapi.com/api/character/${params.id}`
   )
   const character = await res.json()
-  // console.log(character) // log in the terminal (server side)
   return {
     props: {
       character,
